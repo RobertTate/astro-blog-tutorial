de73cd37243acf4f6b2d67c6bae71bf2cb0c850e
RobertTate <robertjosephtate@gmail.com>
2023-06-21 20:35:06 -0500

    add preact component and dark mode

---
+++ src/constants/greetings.js
@@ -0,0 +1,24 @@
+const GREETINGS = [
+  "Hello",
+  "Bonjour",
+  "Hola",
+  "Ciao",
+  "Hallo",
+  "Olá",
+  "Namaste",
+  "Konnichiwa",
+  "Annyeonghaseyo",
+  "Ni hao",
+  "Merhaba",
+  "Salam",
+  "Marhaba",
+  "Szia",
+  "Hej",
+  "Aloha",
+  "Shalom",
+  "Sawubona",
+  "Privet",
+  "Sveiki",
+]
+
+export default GREETINGS;
